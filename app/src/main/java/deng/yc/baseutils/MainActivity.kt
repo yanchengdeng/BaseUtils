73dc7d7cda32a2e98a36329338d216b403e1be18
yancheng <yanchengdeng@gmail.com>
2020-06-01 16:13:39 +0800

    first commend

---
+++ app/src/main/java/deng/yc/baseutils/MainActivity.kt
@@ -0,0 +1,12 @@
+package deng.yc.baseutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
